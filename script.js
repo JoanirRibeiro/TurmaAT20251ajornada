4e163ba205b4b4e60cb426e9b69ee37d9cde965c
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-04-08 23:51:35 +0000

    .

--- script.js
+++ script.js
@@ -22,4 +22,10 @@ document.addEventListener('DOMContentLoaded', function(){
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
     })
 
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
 })
